702f29653c9bcd6ead605d0125a54148b59c2c79
bieniucieniu <mikibien@outlook.com>
2023-03-12 21:00:55 +0100

    add and init firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { Breakout } from "./components/Breakout";
-import {} from "./index.css";
+import "./index.css";
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJeF_fin_zPcrxK6RWmu_BcJ2xTp19Pn8",
+  authDomain: "breakout-32604.firebaseapp.com",
+  projectId: "breakout-32604",
+  storageBucket: "breakout-32604.appspot.com",
+  messagingSenderId: "528439443559",
+  appId: "1:528439443559:web:47fe38c6c3427b23f094c2",
+  measurementId: "G-T4N4VR9CTK",
+};
+
+const firebase = initializeApp(firebaseConfig);
 
 export const App = () => {
   return (
