a0ecb108382c4c312a9c66a276bf45f17553f42c
bieniucieniu <mikibien@outlook.com>
2023-03-17 13:11:49 +0100

    fixing not pausing on lives == 0

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -37,8 +37,11 @@ export const useStorage = create<Storage>((set) => ({
   lives: 3,
   removeLife: () =>
     set((state) => {
-      if (state.lives <= 1) state.setGameStage("over");
-      if (state.lives > 1) {
+      if (state.lives <= 1) {
+        state.setGameStage("over");
+        state.setPause(true);
+      }
+      if (state.lives > 0) {
         return { lives: state.lives - 1 };
       }
       return {};
