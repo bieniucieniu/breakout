36b8d9f279429ef5ed59aa9861e11772a2856c1f
bieniucieniu <mikibien@outlook.com>
2023-03-17 13:09:22 +0100

    remake controlls swap

--- src/components/Breakout.tsx
+++ src/components/Breakout.tsx
@@ -17,70 +17,73 @@ export const Breakout = () => {
       setPaddleControlls: state.setPaddleControlls,
     })
   );
-
-  const startMenuControlls = {
-    onKeyDown: (e: KeyboardEvent) => {
-      switch (e.code) {
-        case "Space":
-          switchPaused();
-          if (gameStage === "starting") {
-            setGameStage("playing");
-          }
-          break;
-      }
+  const controlls = {
+    starting: {
+      onKeyDown: (e: KeyboardEvent) => {
+        switch (e.code) {
+          case "Space":
+            switchPaused();
+            if (gameStage === "starting") {
+              setGameStage("playing");
+            }
+            break;
+        }
+      },
     },
-  };
 
-  const gameControlls = {
-    onKeyDown: (e: KeyboardEvent) => {
-      switch (e.code) {
-        case "Space":
-        case "Escape":
-          switchPaused();
-          break;
-        case "KeyA":
-        case "ArrowLeft":
+    playing: {
+      onKeyDown: (e: KeyboardEvent) => {
+        switch (e.code) {
+          case "Space":
+          case "Escape":
+            switchPaused();
+            break;
+          case "KeyA":
+          case "ArrowLeft":
+            setPaddleControlls({ left: true });
+            break;
+          case "KeyD":
+          case "ArrowRight":
+            setPaddleControlls({ right: true });
+            break;
+        }
+      },
+      onKeyUp: (e: KeyboardEvent) => {
+        switch (e.code) {
+          case "KeyA":
+          case "ArrowLeft":
+            setPaddleControlls({ left: false });
+            break;
+          case "KeyD":
+          case "ArrowRight":
+            setPaddleControlls({ right: false });
+            break;
+        }
+      },
+      onTouchStart: (e: TouchEvent) => {
+        const touch = e.touches[0];
+        if (touch.clientX < window.innerWidth / 2) {
           setPaddleControlls({ left: true });
-          break;
-        case "KeyD":
-        case "ArrowRight":
+        } else {
           setPaddleControlls({ right: true });
-          break;
-      }
-    },
-    onKeyUp: (e: KeyboardEvent) => {
-      switch (e.code) {
-        case "KeyA":
-        case "ArrowLeft":
+        }
+      },
+      onTouchEnd: (e: TouchEvent) => {
+        if (!e.touches.length) {
+          setPaddleControlls({ left: false, right: false });
+          return;
+        }
+        const touch = e.changedTouches[0];
+        if (touch.clientX < window.innerWidth / 2) {
           setPaddleControlls({ left: false });
-          break;
-        case "KeyD":
-        case "ArrowRight":
+        } else {
           setPaddleControlls({ right: false });
-          break;
-      }
+        }
+      },
     },
-    onTouchStart: (e: TouchEvent) => {
-      const touch = e.touches[0];
-      if (touch.clientX < window.innerWidth / 2) {
-        setPaddleControlls({ left: true });
-      } else {
-        setPaddleControlls({ right: true });
-      }
-    },
-    onTouchEnd: (e: TouchEvent) => {
-      if (e.touches.length) {
-        setPaddleControlls({ left: false, right: false });
-        return;
-      }
-      const touch = e.changedTouches[0];
-      if (touch.clientX < window.innerWidth / 2) {
-        setPaddleControlls({ left: false });
-      } else {
-        setPaddleControlls({ right: false });
-      }
-    },
+    over: {},
   };
+
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -91,21 +94,14 @@ export const Breakout = () => {
   }, [paused]);
 
   return (
-    <div
-      ref={ref}
-      className={breakout}
-      tabIndex={0}
-      {...(gameStage === "starting" ? startMenuControlls : gameControlls)}
-    >
-      {gameStage === "over" ? (
-        <GameOver />
-      ) : (
-        <>
-          {gameStage === "starting" ? <StartMenu /> : <PauseMenu />}
-          <GameNavigation />
-          <Game className={game} cameraPosition={[0, 0, 64]} />
-        </>
-      )}
+    <div ref={ref} className={breakout} tabIndex={0} {...controlls[gameStage]}>
+      {
+        { starting: <StartMenu />, playing: <PauseMenu />, over: <GameOver /> }[
+          gameStage
+        ]
+      }
+      <GameNavigation />
+      <Game className={game} cameraPosition={[0, 0, 64]} />
     </div>
   );
 };
