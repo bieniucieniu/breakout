6542f4903b640831b711717103c4f90391712e5c
bieniucieniu <mikibien@outlook.com>
2023-03-23 18:50:45 +0100

    in progress

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,6 +1,6 @@
 import { Link } from "wouter";
 import { center, title } from "./styles/basicStyles.css";
-import { AuthButton } from "./Auth";
+// import { AuthButton } from "./Auth";
 
 export const MainMenu = () => {
   return (
@@ -9,7 +9,7 @@ export const MainMenu = () => {
         game
       </Link>
       <br />
-      <AuthButton />
+      {/* <AuthButton /> */}
     </div>
   );
 };
