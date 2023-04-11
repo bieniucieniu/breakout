e35bdaf6df2c927e0fb13a60e17c433389784ce7
bieniucieniu <mikibien@outlooj.com>
2023-04-11 21:01:06 +0200

    add "your score" on game over

--- src/components/GameOverMenu.tsx
+++ src/components/GameOverMenu.tsx
@@ -1,11 +1,17 @@
 import { menuBG, key } from "./styles/gameMenu.css";
 import { centeredTitle } from "./styles/basicStyles.css";
+import { useStorage } from "../hooks/useStorage";
+import { useEffect, useRef } from "react";
 
 export const GameOverMenu = () => {
+  const lastScore = useStorage((state) => state.lastScore);
+
   return (
     <div className={menuBG}>
       <h1 className={centeredTitle}>
         Game Over <br />
+        your score: {lastScore}
+        <br />
         Press <span className={key}>Space</span> /<br />
         <span className={key}>tap screen</span> <br />
         to restart
