038ce0ee8dd8e8d6905e0d5f6b4aee7a04228bb4
bieniucieniu <mikibien@outlooj.com>
2023-04-11 21:11:16 +0200

    remove unused prop and tabIndex  which may cause error/bug

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,23 +1,11 @@
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/p2";
 import { useState } from "react";
 import { useStorage } from "../../hooks/useStorage";
 import { useWindowFocus } from "../../hooks/useWindowFocus";
 import { Scene } from "./Scene";
-import type { KeyboardEvent, TouchEvent } from "react";
 
-export default ({
-  className,
-  contorlls,
-}: {
-  className?: string;
-  contorlls?: {
-    onKeyDown?: (e: KeyboardEvent) => void;
-    onKeyUp?: (e: KeyboardEvent) => void;
-    onTouchStart?: (e: TouchEvent) => void;
-    onTouchEnd?: (e: TouchEvent) => void;
-  };
-}) => {
+export default ({ className }: { className?: string }) => {
   const config = useStorage((state) => state.config.game);
   const paused = useStorage((state) => state.paused);
   const [windowFocused, setWindowFocused] = useState(true);
@@ -31,8 +19,6 @@ export default ({
     <Canvas
       className={className}
       camera={{ position: config.camera.position.default }}
-      {...contorlls}
-      tabIndex={0}
     >
       <Physics
         normalIndex={2}
