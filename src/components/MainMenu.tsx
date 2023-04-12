52a4b900ae61e0c8252f5f4336cf3c7be1d15289
bieniucieniu <mikibien@outlooj.com>
2023-04-12 23:05:48 +0200

    main menu styles

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,17 +1,17 @@
 import { Link } from "wouter";
-import { center, title } from "./styles/basicStyles.css";
+import { title } from "./styles/basicStyles.css";
 import { Auth } from "./Auth";
+import { mainMenu } from "./styles/mainMenu.css";
 
 export const MainMenu = () => {
   return (
-    <div className={center}>
+    <div className={mainMenu}>
       <Link href="/breakout" className={title}>
         game
       </Link>
       <Link href="/scoreboard" className={title}>
         scoreboard
       </Link>
-      <br />
       <Auth />
     </div>
   );
---
+++ src/components/styles/mainMenu.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+import { center } from "./basicStyles.css";
+
+export const mainMenu = style([
+  center,
+  {
+    display: "grid",
+    gridTemplateRows: "repeat(4, minmax(0, 1fr))",
+    gap: "1rem",
+  },
+]);
