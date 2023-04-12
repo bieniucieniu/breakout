1ddb4b2c4c8ced8f231f30fee02a9dbbb3ae9a93
bieniucieniu <mikibien@outlooj.com>
2023-04-12 18:27:24 +0200

    add basic scoreboard

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,13 +1,33 @@
 import { Auth } from "./Auth";
 import { scoreboardNav } from "./styles/scoreboardNav.css";
+import { useScores } from "../firebase/scoreStorage";
 
 export const Scoreboard = () => {
+  const { scores, loading, error } = useScores({});
+
   return (
     <>
       <nav className={scoreboardNav}>
         <h1>Scoreboard</h1>
         <Auth />
       </nav>
+      <div>
+        <h2>Leaderboard</h2>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        {scores && (
+          <ol>
+            {scores.docs.map((score) => {
+              const s = score.data();
+              return (
+                <li key={score.id}>
+                  <span>{`${s.score}\t${s.name}\t${s.date.toDate()}`}</span>
+                </li>
+              );
+            })}
+          </ol>
+        )}
+      </div>
     </>
   );
 };
