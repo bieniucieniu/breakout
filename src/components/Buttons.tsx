3073f7b541800611d21e0a25ea5a41ff900c6558
bieniucieniu <mikibien@outlooj.com>
2023-04-12 23:05:56 +0200

    add class name prop

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -4,12 +4,14 @@ import { button, linkButton } from "./styles/basicComponents.css";
 export const Button = ({
   name,
   onClick,
+  className,
 }: {
   name?: string;
   onClick?: () => void;
+  className?: string;
 }) => {
   return (
-    <button className={button} onClick={onClick}>
+    <button className={`${button} ${className}`} onClick={onClick}>
       {name}
     </button>
   );
@@ -19,16 +21,18 @@ export const LinkButton = ({
   name,
   onClick,
   href,
+  className,
 }: {
   name?: string;
   onClick?: () => void;
   href: string;
+  className?: string;
 }) => {
   const [_location, setLocation] = useLocation();
 
   return (
     <button
-      className={linkButton}
+      className={`${linkButton} ${className}`}
       onClick={() => {
         onClick && onClick();
         setLocation(href);
