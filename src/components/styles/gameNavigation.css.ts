8c9faff4784760d6453e05e92619328128949756
bieniucieniu <mikibien@outlooj.com>
2023-04-12 22:11:13 +0200

    update styles

--- src/components/styles/gameNavigation.css.ts
+++ src/components/styles/gameNavigation.css.ts
@@ -6,11 +6,3 @@ export const gameNav = style([navbar]);
 globalStyle(`.${gameNav} > *`, {
   fontSize: "1.3rem",
 });
-
-globalStyle(`.${gameNav} > *:nth-child(3)`, {
-  "@media": {
-    "screen and (max-width: 600px)": {
-      gridColumn: "1 / 3",
-    },
-  },
-});
--- src/components/styles/navbar.css.ts
+++ src/components/styles/navbar.css.ts
@@ -3,19 +3,19 @@ import { style } from "@vanilla-extract/css";
 export const navbar = style({
   backgroundColor: "blue",
 
-  padding: ".3rem",
+  padding: "6px",
 
   display: "grid",
   alignItems: "center",
   gridTemplateColumns: "repeat(4, 1fr)",
-  gap: ".5rem",
+  gap: "8px",
 
-  height: "4rem",
+  height: "80px",
 
   "@media": {
     "screen and (max-width: 600px)": {
       gridTemplateColumns: "repeat(2, 1fr)",
-      height: "8rem",
+      height: "160px",
     },
   },
 });
