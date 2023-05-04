78b73ddbc141df486a5507fe8fb06cf30df14938
bieniucieniu <bieniucieniu@outlook.com>
2023-05-04 15:13:38 +0200

    ful collison with paddle

--- src/components/breakoutNormal/Ball.tsx
+++ src/components/breakoutNormal/Ball.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useStorage } from "../../storage";
 import { useFrame } from "@react-three/fiber";
+import { Vector2 } from "three";
 
 export const Ball = ({
   position,
@@ -16,8 +17,9 @@ export const Ball = ({
   const bricks = useStorage((state) => state.bricks);
   const removeLife = useStorage((state) => state.removeLife);
   const ref = useRef<THREE.Mesh>(null!);
-  const vector = useRef([0.1, 0.1] as [number, number]);
-
+  const vector = useRef(
+    new Vector2(ball.speed * Math.sqrt(2), ball.speed * Math.sqrt(2))
+  );
   useFrame(() => {
     const paddlePosition = paddlePositionRef.current;
     const paddleSize = config.game.paddle.args;
@@ -25,35 +27,43 @@ export const Ball = ({
     const ballRadius = ball.radius;
     if (
       ballPosition.x > config.game.args[0] / 2 - ballRadius &&
-      vector.current[0] > 0
+      vector.current.x > 0
     )
-      vector.current[0] *= -1;
+      vector.current.x *= -1;
 
     if (
       ballPosition.x < -config.game.args[0] / 2 + ballRadius &&
-      vector.current[0] < 0
+      vector.current.x < 0
     )
-      vector.current[0] *= -1;
+      vector.current.x *= -1;
 
     if (
       ballPosition.y > config.game.args[1] / 2 - ballRadius &&
-      vector.current[1] > 0
+      vector.current.y > 0
     )
-      vector.current[1] *= -1;
+      vector.current.y *= -1;
 
     if (
       ballPosition.y < -config.game.args[1] / 2 + ballRadius &&
-      vector.current[1] < 0
+      vector.current.y < 0
     )
-      vector.current[1] *= -1;
+      removeLife();
 
-    // ballPosition.x > paddlePosition.x - paddleSize[0] / 2 - ballRadius &&
-    // ballPosition.x < paddlePosition.x + paddleSize[0] / 2 + ballRadius &&
-    // ballPosition.y > paddlePosition.y - paddleSize[1] / 2 - ballRadius &&
-    // ballPosition.y < paddlePosition.y + paddleSize[1] / 2 + ballRadius
+    if (
+      ballPosition.x > paddlePosition.x - paddleSize[0] / 2 - ballRadius &&
+      ballPosition.x < paddlePosition.x + paddleSize[0] / 2 + ballRadius &&
+      ballPosition.y > paddlePosition.y - paddleSize[1] / 2 - ballRadius &&
+      ballPosition.y < paddlePosition.y + paddleSize[1] / 2 + ballRadius &&
+      vector.current.y < 0
+    ) {
+      const r = (ballPosition.x - paddlePosition.x) / (paddleSize[0] / 2);
 
-    ref.current.position.x += vector.current[0];
-    ref.current.position.y += vector.current[1];
+      vector.current.y *= -1;
+      vector.current.rotateAround(new Vector2(0, 0), r);
+    }
+    console.log(vector.current.length());
+    ref.current.position.x += vector.current.x;
+    ref.current.position.y += vector.current.y;
   });
 
   return (
