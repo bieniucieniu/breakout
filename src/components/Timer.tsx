b053a69f1cf2e07e03d5335ebdc4d140c754cca3
bieniucieniu <bieniucieniu@outlook.com>
2023-05-05 20:59:05 +0200

    move timer to component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { display } from "./styles/basicComponents.css";
-import { useTimer } from "../functions/useTimer";
+import { useStorage } from "../storage";
 
 type props = {
   start?: number;
-  delta?: number;
   end?: number;
+  delta?: number;
   onTime?: [onMs: number, func: (current: number, delta: number) => void][];
   onTick?: (current: number, delta: number) => void;
 } & React.HTMLProps<HTMLDivElement>;
 
 export const Timer = ({
   start = 0,
-  onTime,
+  end = Infinity,
+  onTime = [],
   delta = 1,
   onTick,
   className,
 }: props) => {
-  const time = useTimer({ start, onTime, delta, onTick });
+  const paused = useStorage((state) => state.paused);
+  const [time, setTime] = useState(start);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (paused) return;
+      if (time >= end) clearInterval(interval);
+
+      onTick && onTick(time, delta);
+
+      onTime.forEach(([onMs, func]) => {
+        if (time === onMs) {
+          func(time, delta);
+        }
+      });
+      setTime(time + delta);
+    }, delta);
+
+    return () => clearInterval(interval);
+  }, []);
   return <div className={`${display} ${className}`}>{time}</div>;
 };
