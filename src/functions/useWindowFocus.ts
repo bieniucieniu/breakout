a1855d8cd12d3949295af7503488b16ed4162cdb
bieniucieniu <bieniucieniu@outlook.com>
2023-05-07 21:33:06 +0200

    make everythink optional

--- src/functions/useWindowFocus.ts
+++ src/functions/useWindowFocus.ts
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 
-export const useWindowFocus = (onFocus: () => void, onBlur: () => void) => {
+export const useWindowFocus = (onFocus?: () => void, onBlur?: () => void) => {
   useEffect(() => {
-    window.addEventListener("focus", onFocus);
-    window.addEventListener("blur", onBlur);
+    if (onFocus) window.addEventListener("focus", onFocus);
+    if (onBlur) window.addEventListener("blur", onBlur);
 
     return () => {
-      window.removeEventListener("focus", onFocus);
-      window.removeEventListener("blur", onBlur);
+      if (onFocus) window.removeEventListener("focus", onFocus);
+      if (onBlur) window.removeEventListener("blur", onBlur);
     };
   }, []);
 };
