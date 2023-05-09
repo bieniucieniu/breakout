500cf28c588dc371348c7790e8ce92d40e0e802b
bieniucieniu <bieniucieniu@outlook.com>
2023-05-09 12:31:34 +0200

    add touch controlls

--- src/components/breakoutNormal/Paddle.tsx
+++ src/components/breakoutNormal/Paddle.tsx
@@ -1,6 +1,53 @@
 import { useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useStorage } from "../../storage";
+
+function isTouchDevice() {
+  return "ontouchstart" in window || navigator.maxTouchPoints > 0;
+}
+
+const touchControls = () => {
+  const ref = useRef<{ x: number | undefined; y: number | undefined }>({
+    x: undefined,
+    y: undefined,
+  });
+
+  const touch = {
+    onTouchStart: (e: TouchEvent) => {
+      e.preventDefault();
+
+      ref.current.x = e.touches[0].clientX;
+      ref.current.y = e.touches[0].clientY;
+    },
+
+    onTouchMove: (e: TouchEvent) => {
+      e.preventDefault();
+
+      ref.current.x = e.touches[0].clientX;
+      ref.current.y = e.touches[0].clientY;
+    },
+
+    onTouchEnd: (e: TouchEvent) => {
+      e.preventDefault();
+
+      ref.current.x = undefined;
+      ref.current.y = undefined;
+    },
+  };
+
+  useEffect(() => {
+    window.addEventListener("touchstart", touch.onTouchStart);
+    window.addEventListener("touchmove", touch.onTouchMove);
+    window.addEventListener("touchend", touch.onTouchEnd);
+    return () => {
+      window.removeEventListener("touchstart", touch.onTouchStart);
+      window.removeEventListener("touchmove", touch.onTouchMove);
+      window.removeEventListener("touchend", touch.onTouchEnd);
+    };
+  }, []);
+
+  return ref.current;
+};
 
 export const Paddle = ({
   positionRef,
@@ -13,6 +60,11 @@ export const Paddle = ({
     paddle: state.config.game.paddle,
     args: state.config.game.args,
   }));
+  const { paused, gameStage } = useStorage((state) => ({
+    paused: state.paused,
+    gameStage: state.gameStage,
+  }));
+  const touch = touchControls();
 
   const ref = useRef<THREE.Mesh>(null!);
 
@@ -20,12 +72,24 @@ export const Paddle = ({
   const vector = useRef([0, 0, 0] as [number, number, number]);
   const maxSpeed = [paddle.maxSpeed.x, paddle.maxSpeed.y];
   useFrame(({ mouse }, delta) => {
-    vector.current = [
-      ((mouse.x * viewport.width) / 2 - ref.current.position.x) * maxSpeed[0],
-      ((mouse.y * viewport.height) / 2 - ref.current.position.y) * maxSpeed[1],
-      0,
-    ];
+    if (paused || gameStage !== "playing") return;
+    console.log(touch);
 
+    if (touch.x !== undefined && touch.y !== undefined) {
+      vector.current = [
+        ((touch.x * viewport.width) / 2 - ref.current.position.x) * maxSpeed[0],
+        ((touch.y * viewport.height) / 2 - ref.current.position.y) *
+          maxSpeed[1],
+        0,
+      ];
+    } else {
+      vector.current = [
+        ((mouse.x * viewport.width) / 2 - ref.current.position.x) * maxSpeed[0],
+        ((mouse.y * viewport.height) / 2 - ref.current.position.y) *
+          maxSpeed[1],
+        0,
+      ];
+    }
     if (vector.current[1] > maxSpeed[1]) vector.current[1] = maxSpeed[1];
     if (vector.current[1] < -maxSpeed[1]) vector.current[1] = -maxSpeed[1];
 
