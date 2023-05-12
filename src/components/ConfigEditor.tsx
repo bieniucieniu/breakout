ea70d12881e13ee1fd0b39f6548fb5e83505814a
bieniucieniu <bieniucieniu@outlook.com>
2023-05-13 00:19:12 +0200

    change to switch case

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -12,10 +12,10 @@ import {
 import { Button, LinkButton } from "./Buttons";
 import defaultConfig from "../defaultConfig";
 
-type ConfigTypes = String | Number | Boolean | Obj;
+type ConfigTypes = String | Number | Boolean;
 
 type Obj = {
-  [key: string]: ConfigTypes;
+  [key: string]: ConfigTypes | Obj;
 };
 
 const ArrayModule = ({}: {}) => {};
@@ -28,48 +28,56 @@ const PrimitivesModule = ({
   parent: Obj;
 }) => {
   if (parent[keyToElement] === undefined) return <p>error</p>;
-  if (typeof parent[keyToElement] === "string") {
-    const [input, setInput] = useState(parent[keyToElement] as string);
-    const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInput(e.target.value);
-      parent[keyToElement] = e.target.value;
-    };
 
-    return (
-      <p className={primitivesModule}>
-        <span>{keyToElement}</span>
-        <input type="text" value={input} onChange={handlechange} />
-      </p>
-    );
-  }
-  if (typeof parent[keyToElement] === "number") {
-    const [input, setInput] = useState(parent[keyToElement] as number);
-    const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInput(Number(e.target.value));
-      parent[keyToElement] = Number(e.target.value);
-    };
+  const [input, setInput] = useState(parent[keyToElement] as ConfigTypes);
+  let handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    parent[keyToElement] = e.target.value;
+  };
 
-    return (
-      <p className={primitivesModule}>
-        <span>{keyToElement}</span>
-        <input type="number" value={input} onChange={handlechange} />
-      </p>
-    );
-  }
-  if (typeof parent[keyToElement] === "boolean") {
-    const [input, setInput] = useState(parent[keyToElement] as boolean);
-    const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInput(e.target.checked);
-      parent[keyToElement] = e.target.checked;
-    };
+  switch (typeof parent[keyToElement]) {
+    case "string":
+      return (
+        <p className={primitivesModule}>
+          <span>{keyToElement}</span>
+          <input type="text" value={input as string} onChange={handlechange} />
+        </p>
+      );
+    case "number": {
+      handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(Number(e.target.value));
+        parent[keyToElement] = Number(e.target.value);
+      };
 
-    return (
-      <p className={primitivesModule}>
-        <span>{keyToElement}</span>
-        <input type="checkbox" checked={input} onChange={handlechange} />;
-      </p>
-    );
+      return (
+        <p className={primitivesModule}>
+          <span>{keyToElement}</span>
+          <input
+            type="number"
+            value={input as number}
+            onChange={handlechange}
+          />
+        </p>
+      );
+    }
+    case "boolean": {
+      handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.checked);
+        parent[keyToElement] = e.target.checked;
+      };
+      return (
+        <p className={primitivesModule}>
+          <span>{keyToElement}</span>
+          <input
+            type="checkbox"
+            checked={input as boolean}
+            onChange={handlechange}
+          />
+        </p>
+      );
+    }
   }
+
   return <div>error pre</div>;
 };
 
