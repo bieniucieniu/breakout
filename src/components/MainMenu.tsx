44f0e54b0188bd3f79451b8c21ea8729364fe441
bieniucieniu <bieniucieniu@outlook.com>
2023-05-12 15:12:06 +0200

    add config sidebar and styling for for content

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,32 +1,50 @@
 import { Link } from "wouter";
-import { title, spaceItemsVer } from "./styles/basicStyles.css";
+import { title } from "./styles/basicStyles.css";
 import { Auth } from "./Auth";
-import { mainMenu } from "./styles/mainMenu.css";
+import {
+  configButton,
+  configSideBarHidden,
+  configSideBarVisble,
+  mainMenu,
+  mainMenuContent,
+  mainMenuWraper,
+} from "./styles/mainMenu.css";
 import { useStorage } from "../storage";
 import { GameTypeSelector } from "./GameTypeSelector";
 import { ConfigEditor } from "./ConfigEditor";
+import { Button } from "./Buttons";
+import { useState } from "react";
 
 export const MainMenu = () => {
   const gameType = useStorage((state) => state.gameType);
+  const [showConfig, setShowConfig] = useState(false);
   return (
-    <div className={mainMenu}>
-      <GameTypeSelector />
-      <span className={spaceItemsVer}>
-        <Link
-          href={gameType === "gravity" ? "/breakoutGravity" : "/breakoutNormal"}
-          className={title}
-        >
-          play
-        </Link>
-        <span style={{ fontSize: "1.5rem", opacity: ".5" }}>{gameType}</span>
-      </span>
-      <Link href="/scoreboard" className={title}>
-        scoreboard
-      </Link>
-      <Link href="/config" className={title}>
-        config
-      </Link>
-      <Auth />
+    <div className={mainMenuWraper}>
+      <div className={showConfig ? configSideBarVisble : configSideBarHidden}>
+        <ConfigEditor back={() => setShowConfig(false)} />
+      </div>
+
+      <div className={mainMenu}>
+        <div className={mainMenuContent}>
+          <Link
+            href={
+              gameType === "gravity" ? "/breakoutGravity" : "/breakoutNormal"
+            }
+            className={title}
+          >
+            play
+          </Link>
+          <span style={{ fontSize: "1.5rem", opacity: ".5" }}>{gameType}</span>
+          <GameTypeSelector />
+          <Link href="/scoreboard" className={title}>
+            scoreboard
+          </Link>
+          <Auth />
+        </div>
+        <div className={configButton}>
+          <Button name="config" onClick={() => setShowConfig(!showConfig)} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/styles/mainMenu.css.ts
+++ src/components/styles/mainMenu.css.ts
@@ -1,11 +1,77 @@
-import { style } from "@vanilla-extract/css";
-import { center } from "./basicStyles.css";
+import { globalStyle, style } from "@vanilla-extract/css";
 
-export const mainMenu = style([
-  center,
+export const mainMenuWraper = style({
+  position: "relative",
+  overflow: "hidden",
+  width: "100vw",
+  height: "100vh",
+});
+
+export const mainMenu = style({
+  display: "grid",
+  width: "100%",
+  height: "100%",
+
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridTemplateRows: "1fr",
+});
+export const mainMenuContent = style({
+  gridColumn: "1 / 3",
+
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "auto",
+  gap: "1rem",
+  height: "fit-content",
+});
+
+export const configButton = style({
+  gridColumn: "3 / 4",
+  display: "flex",
+  flexDirection: "column",
+  justifySelf: "end",
+  alignSelf: "end",
+  height: "fit-content",
+  width: "30rem",
+  padding: "1.5rem",
+});
+
+globalStyle(`${configButton} > button`, {
+  borderStyle: "solid",
+  borderColor: "#00f",
+});
+
+export const configSideBar = style({
+  zIndex: 100,
+  position: "absolute",
+  right: "0",
+  width: "30rem",
+  maxWidth: "100vw",
+  backgroundColor: "black",
+  borderLeft: "3px solid #00f",
+  transition: "transform 0.3s ease-in-out, visibility 0.3s ease-in-out",
+
+  "@media": {
+    "screen and (max-width: 32rem)": {
+      width: "100vw",
+      borderLeft: "none",
+    },
+  },
+});
+
+export const configSideBarVisble = style([
+  configSideBar,
   {
-    display: "flex",
-    gap: "1rem",
-    flexDirection: "column",
+    visibility: "visible",
+    transform: "translateX(0)",
+  },
+]);
+export const configSideBarHidden = style([
+  configSideBar,
+  {
+    visibility: "hidden",
+    transform: "translateX(100%)",
   },
 ]);
