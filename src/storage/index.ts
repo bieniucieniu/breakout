6e2247f70711a65221f1468da5ffb449336d6433
bieniucieniu <mikibien@outlook.com>
2023-12-17 21:22:04 +0100

    add persist middleware to store

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import defaultConfig from "@/defaultConfig";
 
+import { persist } from "zustand/middleware";
+
 type Storage = {
   paused: boolean;
   setPause: (paused: boolean) => void;
@@ -31,77 +33,83 @@ type Storage = {
   setLastScore: (prop: { classic: number }) => void;
 };
 
-export const useStorage = create<Storage>((set) => ({
-  paused: false,
-  setPause: (paused) => set(() => ({ paused: paused })),
-  switchPaused: () => set((state) => ({ paused: !state.paused })),
-
-  score: 0,
-  increaseScore: (score) => set((state) => ({ score: state.score + score })),
-
-  time: 0,
-  setTime: (t) => set(() => ({ time: t })),
-
-  lives: 3,
-  removeLive: () =>
-    set((state) => {
-      if (state.lives <= 1) {
-        state.endGame();
-        return { lives: 0 };
-      }
-      if (state.lives > 0) {
-        return { lives: state.lives - 1 };
-      }
-      return {};
-    }),
-  resetlives: () => set((state) => ({ lives: state.config.game.lives })),
-
-  gameStage: "init",
-  setupGame: () =>
-    set(() => ({
+export const useStorage = create<Storage>()(
+  persist(
+    (set) => ({
       paused: false,
+      setPause: (paused) => set(() => ({ paused: paused })),
+      switchPaused: () => set((state) => ({ paused: !state.paused })),
+
       score: 0,
+      increaseScore: (score) =>
+        set((state) => ({ score: state.score + score })),
+
+      time: 0,
+      setTime: (t) => set(() => ({ time: t })),
+
       lives: 3,
+      removeLive: () =>
+        set((state) => {
+          if (state.lives <= 1) {
+            state.endGame();
+            return { lives: 0 };
+          }
+          if (state.lives > 0) {
+            return { lives: state.lives - 1 };
+          }
+          return {};
+        }),
+      resetlives: () => set((state) => ({ lives: state.config.game.lives })),
+
       gameStage: "init",
-    })),
-  startGame: () => {
-    set((state) => {
-      if (state.gameStage === "init") {
-        return { gameStage: "playing", paused: false };
-      }
-      return {};
-    });
-  },
-  endGame: () => {
-    set((state) => {
-      if (state.gameStage === "playing") {
-        return {
-          gameStage: "over",
-          paused: true,
-        };
-      }
-      return {};
-    });
-  },
-
-  config: structuredClone(defaultConfig),
-  setConfig: (config) => set({ config: config }),
-
-  resetGame: () => {
-    set((state) => {
-      if (state.gameStage === "over" || state.gameStage === "playing") {
-        state.setupGame();
-        return { gameStage: "init" };
-      }
-      return {};
-    });
-  },
-
-  lastScore: { classic: 0 },
-  setLastScore: ({ classic }) =>
-    set((state) => ({
-      lastScore: {
-        classic: classic,
+      setupGame: () =>
+        set(() => ({
+          paused: false,
+          score: 0,
+          lives: 3,
+          gameStage: "init",
+        })),
+      startGame: () => {
+        set((state) => {
+          if (state.gameStage === "init") {
+            return { gameStage: "playing", paused: false };
+          }
+          return {};
+        });
       },
-    })),
-}));
+      endGame: () => {
+        set((state) => {
+          if (state.gameStage === "playing") {
+            return {
+              gameStage: "over",
+              paused: true,
+            };
+          }
+          return {};
+        });
+      },
+
+      config: structuredClone(defaultConfig),
+      setConfig: (config) => set({ config: config }),
+
+      resetGame: () => {
+        set((state) => {
+          if (state.gameStage === "over" || state.gameStage === "playing") {
+            state.setupGame();
+            return { gameStage: "init" };
+          }
+          return {};
+        });
+      },
+
+      lastScore: { classic: 0 },
+      setLastScore: ({ classic }) =>
+        set((state) => ({
+          lastScore: {
+            classic: classic,
+          },
+        })),
+    }),
+    { name: "breakout-store" },
+  ),
+);
